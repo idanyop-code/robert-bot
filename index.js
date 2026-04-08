2c4a4dc0ffac90aa1f0759b3055900bb9368a603
Idan <idan@surense.com>
2026-04-08 21:11:45 +0300

    fix puppeteer for railway

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ const openai = new OpenAI({
 
 const client = new Client({
   authStrategy: new LocalAuth({ clientId: 'robert' }),
+  puppeteer: {
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+  },
 });
 
 const OWNER_NUMBER = process.env.OWNER_NUMBER || '';
